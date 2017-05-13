fc07abae313f265c69b2544c4b32de81652f03c1
lex-attax <lex.pavlovic@gmail.com>
2017-05-13 22:53:21 +0200

    Delete Tree.java

--- 30DaysOfCode/day27/day27/Tree.java
+++
@@ -1,8 +0,0 @@
-package day27;
-
-public interface Tree<D extends Comparable> {
-    public boolean isEmpty();
-    public int cardinality();
-    public boolean member(D elt);
-    public day27.NonEmptyBST<D> add(D elt);
-}
